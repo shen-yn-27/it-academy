7b3798976953fdd860530a72a9affc3fa509d743
shen-yn-27 <shen.yn.27@gmail.com>
2025-01-14 20:00:14 +0500

    z ecnfkf

--- erha/мо.js
+++ erha/мо.js
@@ -2,14 +2,18 @@ let title = document.getElementById("title");
 let description = document.getElementById("description");
 let button = document.getElementById("button");
 let list = document.getElementById("list");
+let searchInput = document.getElementById("search");
+let searchButton = document.getElementById("search-button");
+let resetButton = document.getElementById("reset-button");
 let count = 0
+let tasksListHtml = ''
 
 function addTask() {
     let task = document.createElement("div");
     let taskTitle = document.createElement("div");
     let taskDescription = document.createElement("div");
     let deleteButton = document.createElement("button");
-    let taskNumber = document.createElement("input");
+    let taskNumber = document.createElement("div");
     
     function deleteTask() {
         task.remove();
@@ -23,21 +27,43 @@ function addTask() {
     deleteButton.innerHTML = 'Удалить'
 
     count = count + 1
-    taskNumber.innerHTML = `<b>Задча №${ count }</b>`
+    taskNumber.innerHTML = `<b>Задча №${count}</b>`
 
     task.classList.add('task');
     taskTitle.classList.add('taskTitle');
     taskDescription.classList.add('task-description');
     
 
-    task.append(taskTitle, taskDescription);
+    task.append( taskTitle, taskDescription);
 
-    task.append(taskTitle, taskDescription, deleteButton);
+    task.append(taskNumber, taskTitle, taskDescription, deleteButton);
 
     list.append(task);
+
+    tasksListHtml = list.innerHTML;
 
     title.value = ''
     description.value = ''
 }
 
 button.addEventListener("click", addTask)
+
+function searchTask() {
+    let tasks = document.getElementsByClassName('task');
+    for (let task of tasks) {
+        let titles = task.getElementsByClassName('taskTitle');
+        let title = titles[0];
+
+        if (title.innerText.includes(searchInput.value) == false) {
+           task.remove();
+        }
+    }
+}
+
+searchButton.addEventListener("click", searchTask)
+
+function resetTask() {
+    list.innerHTML = tasksListHtml
+}
+    
+resetButton.addEventListener("click", resetTask);
