09abd41a9eb099733e571dc8b0ac5696b8d894c5
shen-yn-27 <shen.yn.27@gmail.com>
2025-02-11 20:00:40 +0500

    w

--- layout/java/women/xch.js
+++ layout/java/women/xch.js
@@ -1,10 +1,14 @@
 let container = document.getElementById('container');
 
 let users = []
+let page = 1
 
 
 async function getUsers() {
 let response = await axios.get('https://kinopoiskapiunofficial.tech/api/v2.2/films?type=TV_SERIES', {
+    params: {
+        page: page,
+    },
     headers: {
         'X-API-KEY': 'd346c82b-bd03-444d-9600-7a7a36f9358c',
     },
@@ -15,16 +19,14 @@ users = response.data.items
 }
 
 function showTasks() {
-
+    container.innerHTML = ''
     for (let user of users) {
-        //Создаём карточку пользователя
         let userCard = document.createElement('div')
         let userAvatar = document.createElement('div')
         let userName = document.createElement('div')
         let userPhone = document.createElement('div')
         let userEmail = document.createElement('div')
 
-        //Добавляем карточке пользвателя класс user-card
         userCard.classList.add('movie')
 
         userAvatar.classList.add('movie__cover-inner')
@@ -37,7 +39,6 @@ function showTasks() {
 
 
         userAvatar.innerHTML = `<img src="${user.posterUrl}" alt="" class="movie__cover">`
-
         userName.innerHTML = user.nameRu
         userPhone.innerHTML = user.ratingKinopoisk
         userEmail.innerHTML = user.year
@@ -46,10 +47,38 @@ function showTasks() {
         userCard.append(userName, userPhone)
         userCard.append(userEmail)
 
-        //Поместить карточку пользователя в конец списка
         container.append(userCard)
     }
 }
 
+
+
+function button() {
+    let buttons = document.getElementsByClassName('page')
+    for (let button of buttons) {
+
+        async function changePage() {
+            page = button.innerText
+            await getUsers()
+
+
+            if (button = 'page = button.innerText') {
+                this.classList.add('active')
+            }
+            else if (button !== 'page = button.innerText') {
+                this.classList.add('disabled')
+            }
+        }
+
+        button.addEventListener('click', changePage)
+    }
+}
+
+
+
 getUsers()
+button()
+
+
+
 
